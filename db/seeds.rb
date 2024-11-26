d10fa165919c687428474ca0b53c09279e46572f
Pauline Metay <metaypauline@gmail.com>
2024-11-26 17:59:18 +0100

    articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do
+  article = Article.create(
+    title: Faker::Quote.famous_last_words,
+    content: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false)
+  )
+end
